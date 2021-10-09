5a550bd32c80f9c809fba69d328767fa21df0729
Jeffrey <hernew@gmail.com>
2021-10-09 11:30:08 -0700

    jschange

--- script.js
+++ script.js
@@ -4,8 +4,66 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('questions')
 const answerButtonsElement = document.getElementById('answer-buttons')
 var sec = 60;
-
+let questions = questionsElement
 let shuffleQuestions, currentQuestionIndex
+
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "What does PHP stand for?",
+  answer: "Hypertext Preprocessor",
+  options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+  ]
+},
+  {
+  numb: 4,
+  question: "What does SQL stand for?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+  {
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]},
+  //the array ends at this close bracket, so the function above that is inside of the array.
+]
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -33,7 +91,7 @@ startTimer()
 console.log('Started')
 startButton.classList.add('hide')
 shuffleQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
+currentQuestionsIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
@@ -99,85 +157,3 @@ function clearStatusClass(element) {
   element.classList.remove ('correct')  
   element.classList.remove ('wrong')
 }
-
-function shuffle(arr) {
-const clone = [...arr]
-return clone.sort (() => Math.random() - 0.5);}
-
-let questions = [
-  {
-  numb: 1,
-  question: "What does HTML stand for?",
-  answer: "Hyper Text Markup Language",
-  options: [
-    "Hyper Text Preprocessor",
-    "Hyper Text Markup Language",
-    "Hyper Text Multiple Language",
-    "Hyper Tool Multi Language"
-  ]
-},
-  {
-  numb: 2,
-  question: "What does CSS stand for?",
-  answer: "Cascading Style Sheet",
-  options: [
-    "Common Style Sheet",
-    "Colorful Style Sheet",
-    "Computer Style Sheet",
-    "Cascading Style Sheet"
-  ]
-},
-  {
-  numb: 3,
-  question: "What does PHP stand for?",
-  answer: "Hypertext Preprocessor",
-  options: [
-    "Hypertext Preprocessor",
-    "Hypertext Programming",
-    "Hypertext Preprogramming",
-    "Hometext Preprocessor"
-  ]
-},
-  {
-  numb: 4,
-  question: "What does SQL stand for?",
-  answer: "Structured Query Language",
-  options: [
-    "Stylish Question Language",
-    "Stylesheet Query Language",
-    "Statement Question Language",
-    "Structured Query Language"
-  ]
-},
-  {
-  numb: 5,
-  question: "What does XML stand for?",
-  answer: "eXtensible Markup Language",
-  options: [
-    "eXtensible Markup Language",
-    "eXecutable Multiple Language",
-    "eXTra Multi-Program Language",
-    "eXamine Multiple Language"
-  ]
-  },
-
-  
-  function startTimer(){
-      console.log('timer suppose to go')
-      var timer = setInterval(function(){
-          sec--;
-
-          document.getElementById('timerDisplay').innerHTML='00:'+ sec;
-          console.log(sec)
-          if (sec <= 0) {
-              clearInterval(timer);
-              alert("Time is up!")
-          }
-      }, 1000);
-  
-    }
-    // document.getElementsByClassName(wrong).addEventListener('click', function() {
-        
-    // });
-    // startTimer();
-]
